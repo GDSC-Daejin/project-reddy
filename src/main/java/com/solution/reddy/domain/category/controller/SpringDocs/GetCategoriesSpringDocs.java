a98a9016f40e6b49cd7de4eaa0e432a5c25937c6
myeongju00 <j51677767@gmail.com>
2024-02-14 20:20:03 +0900

    [feat] : 카테고리 전체 조회, 등록 api 스웨거 작성

---
+++ src/main/java/com/solution/reddy/domain/category/controller/SpringDocs/GetCategoriesSpringDocs.java
@@ -0,0 +1,18 @@
+package com.solution.reddy.domain.category.controller.SpringDocs;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "카테고리 조회 API", description = "카테고리 전체 조회 API 입니다.")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", useReturnTypeSchema = true)
+})
+public @interface GetCategoriesSpringDocs {
+}
