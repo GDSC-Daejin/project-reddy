c87750b80877da298b013361e47a1d8d001b4e73
myeongju00 <j51677767@gmail.com>
2024-02-20 01:14:29 +0900

    [feat] : 기사 업로드 api 수정

--- src/main/java/com/solution/reddy/domain/article/controller/ArticleController.java
+++ src/main/java/com/solution/reddy/domain/article/controller/ArticleController.java
@@ -7,18 +7,17 @@ import com.solution.reddy.domain.article.dto.response.DetailArticleDto;
 import com.solution.reddy.domain.article.service.ArticleService;
 import com.solution.reddy.global.controller.FirstVersionRestController;
 import com.solution.reddy.global.dto.ReddyApiResponse;
-import com.solution.reddy.global.exception.ApiException;
 import com.solution.reddy.global.message.ArticleMessage;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "뉴스 기사 관련 컨트롤러")
@@ -41,8 +40,8 @@ public class ArticleController {
         return ReddyApiResponse.createResponse(article, ArticleMessage.GET_ARTICLE_TITLE_SUCCESS);
     }
 
-    @PostMapping("/article")
-    public ReddyApiResponse<?> postArticle(@RequestBody MultipartFile file){
+    @PostMapping(value = "/article", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ReddyApiResponse<?> postArticle(@ModelAttribute MultipartFile file){
         articleService.uploadFile(file);
         return ReddyApiResponse.createResponse(null, ArticleMessage.ARTICLE_POST_SUCCESS);
     }
